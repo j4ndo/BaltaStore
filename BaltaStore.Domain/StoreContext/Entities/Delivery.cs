05db68e8780a6ffdec8b2a5a360cfbf650dd2ee9
Janderson Fonseca <janderson-fonseca@hotmail.com>
2020-02-29 15:11:02 -0300

    feat: Pedido Modelando o Pedido

--- BaltaStore.Domain/StoreContext/Entities/Delivery.cs
+++ BaltaStore.Domain/StoreContext/Entities/Delivery.cs
@@ -15,5 +15,13 @@ namespace BaltaStore.Domain.StoreContext.Entities
         public DateTime EstimatedDeliveryDate { get; private set; }
         public EDeliveryStatus Status { get; private set; }
 
+        public void Ship(){
+            // Se a Data estimada de entrega for no passado, não entregar
+            Status = EDeliveryStatus.Shipped;
+        }
+        public void Cancel(){
+            // Se o Status jpa estiver entregue, não pode cancelar
+            Status = EDeliveryStatus.Cancelled;
+        }
     }
 }
--- BaltaStore.Domain/StoreContext/Entities/Order.cs
+++ BaltaStore.Domain/StoreContext/Entities/Order.cs
@@ -13,7 +13,6 @@ namespace BaltaStore.Domain.StoreContext.Entities
         public Order(Customer customer)
         {
             Customer = customer;
-            Number = Guid.NewGuid().ToString().Replace("-", "").Substring(0,8);
             CreateDate = DateTime.Now;
             Status = EOrderStatus.Created;
             _items = new List<OrderItem>();
@@ -31,14 +30,49 @@ namespace BaltaStore.Domain.StoreContext.Entities
         { 
             _items.Add(item);
         }
-        public void AddDelivery (Delivery delivery) 
-        { 
-            _deliveries.Add(delivery);
+        
+
+        // Criar um Pedido
+        public void Place() {
+            // Gerar o número do Pedido
+            Number = Guid.NewGuid().ToString().Replace("-", "").Substring(0,8);            
+        }        
+        // Pagar um Pedido
+        public void Pay()
+        {
+            Status = EOrderStatus.Paid;            
         }
 
-        // To Place An Order
-        public void Place() {
+        // Enviar um Pedido
+        public void Ship()
+        {
+            // A cada 5 produtos é uma entrega
+            var deliveries = new List<Delivery>();
+            deliveries.Add(new Delivery(DateTime.Now.AddDays(5)));
+            var count = 1;
 
-        }        
+            foreach (var item in _items)
+            {
+                if (count == 5)
+                {
+                    count = 1;
+                    deliveries.Add(new Delivery(DateTime.Now.AddDays(5)));        
+                } 
+                count++;  
+            }
+            // Envia Todas as Entregas
+            deliveries.ForEach(x => x.Ship());
+            
+            // Adiciona Entregas ao Pedido
+            deliveries.ForEach(x => _deliveries.Add(x));
+            
+        }
+
+        // Cancelar um Pedido
+        public void Cancel()
+        {
+            Status = EOrderStatus.Cancelled;
+            _deliveries.ToList().ForEach(x => x.Cancel());
+        }
     }
 }
