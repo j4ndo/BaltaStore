d5f67478b0370c050da6621ebcefa95f54d4e77f
Janderson Fonseca <janderson-fonseca@hotmail.com>
2020-03-01 21:23:25 -0300

    feat: Simulando o Pedido

--- BaltaStore.Tests/UnitTest1.cs
+++ BaltaStore.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
 using BaltaStore.Domain.StoreContext.Entities;
+using BaltaStore.Domain.StoreContext.ValueObjects;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 namespace BaltaStore.Tests
@@ -9,24 +11,26 @@ namespace BaltaStore.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            // var c = new Customer(
-            //     "Janderson",
-            //     "Fonseca",
-            //     "0123456789",
-            //     "janderson-fonseca@hotmail.com",
-            //     "84999887766",
-            //     "Avenida Salgado Filho, 512"
-            // );
-            // var product = new Product(
-            //     "Moto G7",
-            //     "Smartphone Moto G7",
-            //     "image.png",
-            //     Convert.ToDecimal(999.99),
-            //     100
-            //     );
-            // var order = new Order(c);
-            // var item = new OrderItem(product, Convert.ToDecimal(999.99));
-            // order.AddItem(item);
+        var name = new Name("Janderson", "Fonseca");
+        var document = new Document("0123456789");
+        var email = new Email("janderson-fonseca@hotmail.com");
+        var c = new Customer(name, document, email, "84999887766");
+        var mouse = new Product("Mouse","Rato","image.png", 59.99M, 10);
+        var teclado = new Product("Teclado","teclado","image.png", 159.99M, 10);
+        var impressora = new Product("Impressora","Impressora","image.png", 359.99M, 10);
+        var cadeira = new Product("Cadeira","Cadeira","image.png", 559.99M, 10);
+            
+        var order = new Order(c);
+        order.AddItem(new OrderItem(mouse, 5));
+        order.AddItem(new OrderItem(teclado, 5));
+        order.AddItem(new OrderItem(impressora, 5));
+        order.AddItem(new OrderItem(cadeira, 5));
+
+        // Realiza Pedido
+        order.Place();
+        order.Pay();
+        order.Ship();
+        order.Cancel();
         }
     }
 }
